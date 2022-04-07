55816e50862659f6ae741c38c455b7df72761c5e
liuyao <liuyao@liuyaodeiMac.local>
2022-04-07 14:44:19 +0800

    feat: add parse element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content);
 
@@ -8,12 +13,53 @@ export function baseParse(content: string) {
 
 function parseChildren(context) {
   const nodes: any[] = [];
+  const s = context.source;
+  let node;
 
-  const node = parseInterpolation(context);
+  // 处理 插值
+  if (s.startsWith("{{")) {
+    node = parseInterpolation(context);
+  } else if (s.startsWith("<")) {
+    // 处理 标签
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
+  }
+
+  
 
   nodes.push(node);
-
   return nodes;
+}
+
+// 解析 element
+//   处理标签 <div></div>
+function parseElement(context) {
+  const element = parseTag(context, TagType.Start);
+
+  parseTag(context, TagType.End);
+
+  return element;
+}
+
+function parseTag(context, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+
+  const tag = match[1];
+  // 处理完成的字符串 清除掉
+  // 清除 <div
+  advanceBy(context, match[0].length);
+  // 清除 >
+  advanceBy(context, 1);
+
+  if (type === TagType.End) {
+    return;
+  }
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 
 function parseInterpolation(context) {
